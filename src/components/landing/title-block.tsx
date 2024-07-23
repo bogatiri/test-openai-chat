9cfdb1dd2ef04906e6d4c9952a170092da7f315f
K1ska123 <ro4evalex@gmail.com>
2024-07-23 21:24:15 +0300

    micro-fix)

--- src/components/landing/title-block.tsx
+++ src/components/landing/title-block.tsx
@@ -3,7 +3,7 @@ import Chat from './chat'
 
 const TitleBlock = () => {
 	return (
-		<div className='grid px-10 sm:px-20 md:px-20 lg:px-20 xl:32px 2xl:40px 3xl:60px 4xl:px-80 grid-rows-[0.2fr_1.4fr] xl:mt-28 xl:grid-cols-[1fr_1.4fr] gap-8'>
+		<div className='grid px-10 sm:px-20 md:px-20 lg:px-20 xl:32px 2xl:40px 3xl:60px 4xl:px-80 grid-rows-[0.2fr_1.4fr] xl:grid-rows-1 xl:mt-28 xl:grid-cols-[1fr_1.4fr] gap-8'>
 			<div className='flex flex-col justify-center items-center xl:justify-start my-24 xl:my-36 gap-4 xl:w-[519px]'>
 				<h1 className='font-extrabold text-4xl'>
 					ChatGPT и Midjourney: путь к эффективному решению ваших задач
